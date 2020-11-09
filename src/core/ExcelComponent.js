eca4e563b66f596a74a5fb81ec4a648d70cf59f6
evgeniy <g@mail.com>
2020-11-09 13:45:30 +0200

    classes

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,8 @@
+import { DomListener } from "@core/DomListener";
+
+export class ExcelComponent extends DomListener {
+
+    toHTML() {
+        return ''
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './scss/index.scss';
+import { Excel } from './components/excel/excel';
 
+const excel = new Excel('#app', {
+    components: []
+});
+
+console.log(excel)
