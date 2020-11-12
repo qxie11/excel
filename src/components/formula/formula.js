e205fe19cbda55af467e9b183c9faebf9911c1d9
Evgeniy Ponomarev <41052525+qxie11@users.noreply.github.com>
2020-11-12 15:51:15 +0200

    Finish base framework functional (#3)

--- src/components/formula/formula.js
+++ src/components/formula/formula.js
@@ -1,5 +1,25 @@
 import { ExcelComponent } from "@core/ExcelComponent";
 
 export class Formula extends ExcelComponent {
+    static className = 'excel__formula';
 
+    constructor($root) {
+        super($root, {
+            name: 'Formula',
+            listeners: ['input', 'click']
+        });
+    }
+
+    toHTML() {
+        return `<div class="info">fx</div>
+         <div class="input" contenteditable spellcheck="false"></div>`
+    }
+
+    onInput(e) {
+        console.log('onInput', e);
+    }
+
+    onClick() {
+
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './scss/index.scss';
 import { Excel } from './components/excel/excel';
+import { Header } from '@/components/header/header';
+import { Toolbar } from '@/components/toolbar/toolbar';
+import { Formula } from '@/components/formula/formula';
+import { Table } from '@/components/table/table';
 
 const excel = new Excel('#app', {
-    components: []
+    components: [Header, Toolbar, Formula, Table]
 });
 
-console.log(excel)
+excel.render()
